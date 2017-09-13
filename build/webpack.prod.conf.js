9923c4ffbb23b5612b0efe540ebc859620c32b46
刘军立 <flasher.jun@gmail.com>
2017-09-14 00:10:56 +0800

    modify webpack prod config

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -9,6 +9,9 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 
+var entries = utils.getMultiEntry('./src/pages/**/*.js');
+var chunks = Object.keys(entries);
+
 var env = config.build.env
 
 var webpackConfig = merge(baseWebpackConfig, {
@@ -18,11 +21,11 @@ var webpackConfig = merge(baseWebpackConfig, {
             extract: true
         })
     },
-    devtool: config.build.productionSourceMap ? '#source-map' : false,
+    // devtool: config.build.productionSourceMap ? '#source-map' : false,
     output: {
         path: config.build.assetsRoot,
-        filename: utils.assetsPath('js/[name].[chunkhash].js'),
-        chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
+        filename: utils.assetsPath('js/[name].js'),
+        chunkFilename: utils.assetsPath('js/[id].js')
     },
     plugins: [
         // http://vuejs.github.io/vue-loader/en/workflow/production.html
@@ -37,7 +40,7 @@ var webpackConfig = merge(baseWebpackConfig, {
         }),
         // extract css into its own file
         new ExtractTextPlugin({
-            filename: utils.assetsPath('css/[name].[contenthash].css')
+            filename: utils.assetsPath('css/[name].css')
         }),
         // Compress extracted CSS. We are using this plugin so that possible
         // duplicated CSS from different components can be deduped.
@@ -46,25 +49,6 @@ var webpackConfig = merge(baseWebpackConfig, {
                 safe: true
             }
         }),
-        // generate dist index.html with correct asset hash for caching.
-        // you can customize output by editing /index.html
-        // see https://github.com/ampedandwired/html-webpack-plugin
-        new HtmlWebpackPlugin({
-            filename: config.build.index,
-            template: 'index.html',
-            inject: true,
-            minify: {
-                removeComments: true,
-                collapseWhitespace: true,
-                removeAttributeQuotes: true
-                // more options:
-                // https://github.com/kangax/html-minifier#options-quick-reference
-            },
-            // necessary to consistently work with multiple chunks via CommonsChunkPlugin
-            chunksSortMode: 'dependency'
-        }),
-        // keep module.id stable when vender modules does not change
-        new webpack.HashedModuleIdsPlugin(),
         // split vendor js into its own file
         new webpack.optimize.CommonsChunkPlugin({
             name: 'vendor',
@@ -81,7 +65,7 @@ var webpackConfig = merge(baseWebpackConfig, {
         }),
         // extract webpack runtime and module manifest to its own file in order to
         // prevent vendor hash from being updated whenever app bundle is updated
-        new webpack.optimize.CommonsChunkPlugin({
+        /*new webpack.optimize.CommonsChunkPlugin({
             name: 'manifest',
             chunks: ['vendor']
         }),
@@ -92,7 +76,7 @@ var webpackConfig = merge(baseWebpackConfig, {
                 to: config.build.assetsSubDirectory,
                 ignore: ['.*']
             }
-        ])
+        ])*/
     ]
 })
 
@@ -117,6 +101,20 @@ if (config.build.productionGzip) {
 if (config.build.bundleAnalyzerReport) {
     var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
     webpackConfig.plugins.push(new BundleAnalyzerPlugin())
+}
+
+var pages = utils.getMultiEntry('./src/pages/**/*.html');
+for (var pathname in pages) {
+    // debugger
+    var conf = {
+        filename: pathname + '.html',
+        template: pages[pathname], // 模板路径
+        chunks: pathname === 'index' ? ['vendor', pathname] : [pathname], // 每个html引用的js模块
+        inject: true,
+        hash: true
+    };
+    
+    webpackConfig.plugins.push(new HtmlWebpackPlugin(conf));
 }
 
 module.exports = webpackConfig
