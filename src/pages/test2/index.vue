f4bdc5c60e15d4f3e48cb4be2ab9fcb18ab4053d
刘军立 <flasher.jun@gmail.com>
2017-09-13 23:55:15 +0800

    creat test pages

---
+++ src/pages/test2/index.vue
@@ -0,0 +1,54 @@
+<template>
+    <div id="app">
+        <img src="../../assets/logo.png">
+        <div class="hello">
+            <h1>{{ msg }}</h1>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="test1.html">page1</a></li>
+                <li><a href="test2.html">page2</a></li>
+                <li><a href="test3.html">page3</a></li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+        data() {
+            return {
+                msg: 'Vue multi-page 2',
+            };
+        },
+    };
+</script>
+
+<style>
+    body{background: #FFF4F5}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+    h1, h2 {
+        font-weight: normal;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+</style>
